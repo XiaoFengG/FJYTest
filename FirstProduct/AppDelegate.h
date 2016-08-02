f51c15f7f9ddc8180736e3cdd19ab9334a82db85
XiaoFengG <fengjiayu6800@163.com>
2016-08-02 10:21:09 +0800

    必填项
    第一个程序

---
+++ FirstProduct/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstProduct
+//
+//  Created by 冯佳玉 on 16/8/1.
+//  Copyright © 2016年 冯佳玉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstProduct/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FirstProduct
+//
+//  Created by 冯佳玉 on 16/8/1.
+//  Copyright © 2016年 冯佳玉. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (nonatomic,strong) NSString *name;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
